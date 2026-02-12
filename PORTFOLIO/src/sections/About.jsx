fc837a1f55ecf9f787c7e47f868ec3f15d51b385
Lagadnakul <nakullagad084@gmail.com>
2026-02-12 16:20:10 +0530

    push

--- PORTFOLIO/src/sections/About.jsx
+++ PORTFOLIO/src/sections/About.jsx
@@ -1,3 +1,17 @@
 export const About = () => {
-    return <section id="about"></section>
-}
+    return (
+    <section id="about" className= "py-32 relative overflow-hidden">
+    <div> 
+        <div>
+            {/* Left Column  */}
+            <div>
+                <Span>About Me</Span>
+            </div>
+            <h2>
+                Building immersive web applications with purpose and precision.
+            </h2>
+        </div>
+    </div>
+    </section>
+);
+};
